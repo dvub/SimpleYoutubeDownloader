e8160c25d0d39a9ed4873945579b65f638858c45
dvub <84478450+dvub@users.noreply.github.com>
2021-09-05 10:26:18 -0700

    Add project files.

---
+++ YoutubeDownloader/AppSettings.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace YoutubeDownloader
+{
+    public class AppSettings
+    {
+        public string path { get; set; }
+
+        [JsonIgnore]
+        public static string FILE_NAME = AppContext.BaseDirectory + "appsettings.json";
+
+        [JsonIgnore]
+        public static bool IsCreated
+          => (File.Exists(FILE_NAME));
+
+        /// <summary>
+        /// make new JSON file.
+        /// </summary>
+        /// <returns>new JSON config file</returns>
+        public static AppSettings Create()
+        {
+
+            if (IsCreated)
+            {
+                return Load();
+
+            }
+            if (!Directory.Exists(AppContext.BaseDirectory + @"\save"))
+                Directory.CreateDirectory(AppContext.BaseDirectory + @"\save");
+            var config = new AppSettings()
+            {
+
+                path = ""
+            };
+            JsonSerializerOptions options = new()
+            {
+                WriteIndented = true
+            };
+
+            File.WriteAllBytes(FILE_NAME, JsonSerializer.SerializeToUtf8Bytes(config, options));
+            return config;
+
+        }
+        /// <summary>
+        /// Loads existing JSON file.
+        /// </summary>
+        /// <returns>JSON config file.</returns>
+        public static AppSettings Load()
+        {
+            var readBytes = File.ReadAllBytes(FILE_NAME);
+            var config = JsonSerializer.Deserialize<AppSettings>(readBytes);
+            return config;
+        }
+    }
+}
---
+++ YoutubeDownloader/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+namespace YoutubeDownloader
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            AppSettings appSettings = AppSettings.IsCreated //check for config file
+                ? AppSettings.Load()
+                : AppSettings.Create();
+
+            var youtube = new YoutubeClient();
+            try
+            {
+                string choice = "";
+                while (choice != "2")
+                {
+                    Console.WriteLine("Please enter URL");
+                    string url = Console.ReadLine().Substring(32);
+                    var video = await youtube.Videos.GetAsync(url);
+                    Console.WriteLine($"Now converting: {video.Title} to mp3...");
+                    var streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
+                    Console.WriteLine("Got stream manifest..");
+                    var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+                    Console.WriteLine("Got audio stream from manifest..");
+                    string path = $@"{appSettings.path}{video.Title}.mp3";
+                    if (File.Exists(path))
+                    {
+                        Console.WriteLine("Error: A file already exists with that name!");
+                        return;
+                    }
+                    await youtube.Videos.Streams.DownloadAsync(streamInfo, path);
+                    Console.WriteLine("Finished downloading!");
+                    Console.WriteLine("\nSelect an option:");
+                    Console.WriteLine("1. Download another mp3");
+                    Console.WriteLine("2. Exit");
+                    choice = Console.ReadLine();
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred: {e} \n Shutting Down..");
+                return;
+            }
+        }
+
+    }
+}
